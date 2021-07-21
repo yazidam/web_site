f63708975c9b49879916e16759c988ab0074a206
MEJRI AhmedYazid <ahmedyazid.mejri@esprit.tn>
2021-07-21 21:15:19 +0100

    FAQ using collapse component

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import About from "../components/home/About";
 import Feature from "../components/home/Feature";
+import Faq from "../components/home/Faq";
+
 import Hero from "../components/home/Hero";
 import Works from "../components/home/Works";
 
@@ -11,6 +13,7 @@ const Home = () => {
       <About />
       <Feature />
       <Works />
+      <Faq />
     </div>
   );
 };
