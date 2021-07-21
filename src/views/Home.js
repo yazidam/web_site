fce15fac0cd9d9849ab28e15dab69a23e14c8fb2
MEJRI AhmedYazid <ahmedyazid.mejri@esprit.tn>
2021-07-21 21:38:45 +0100

    Plans using lists component

--- src/views/Home.js
+++ src/views/Home.js
@@ -5,6 +5,7 @@ import Faq from "../components/home/Faq";
 
 import Hero from "../components/home/Hero";
 import Works from "../components/home/Works";
+import Princing from "../components/home/Princing";
 
 const Home = () => {
   return (
@@ -14,6 +15,7 @@ const Home = () => {
       <Feature />
       <Works />
       <Faq />
+      <Princing />
     </div>
   );
 };
