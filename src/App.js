61aa1c6a608e1a408b49b3b6b5b162e29b61b0ab
MEJRI AhmedYazid <ahmedyazid.mejri@esprit.tn>
2021-07-21 18:55:10 +0100

    Adding Carousel

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import "antd/dist/antd.css";
 import { Layout } from "antd";
 import Headerr from "./components/Headerr";
+import Home from "./views/Home";
 
 const { Header, Content, Footer } = Layout;
 function App() {
@@ -11,6 +12,9 @@ function App() {
       <Header>
         <Headerr />
       </Header>
+      <Content>
+        <Home />
+      </Content>
     </Layout>
   );
 }
