09091b2f53d4844174864dc123716f7a9156fa25
MEJRI AhmedYazid <ahmedyazid.mejri@esprit.tn>
2021-07-21 19:16:50 +0100

    Adding about section

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import About from "../components/home/About";
 import Hero from "../components/home/Hero";
 
 const Home = () => {
   return (
     <div className="main">
       <Hero />
+      <About />
     </div>
   );
 };
