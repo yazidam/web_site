8325520e1e019ba831b523996f966ab3c333953f
MEJRI AhmedYazid <ahmedyazid.mejri@esprit.tn>
2021-07-22 16:42:16 +0100

    Responsive menu using drawer component

--- src/components/Headerr.js
+++ src/components/Headerr.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { Anchor } from "antd";
+import { Anchor, Drawer, Button } from "antd";
 
 const { Link } = Anchor;
 const Headerr = () => {
+  const [visible, setVisible] = useState(false);
+  const showDrawer = () => {
+    setVisible(true);
+  };
+  const onClose = () => {
+    setVisible(false);
+  };
   return (
     <div className="container-fluid">
       <div className="header">
@@ -11,16 +18,39 @@ const Headerr = () => {
           <i className="fas fa-bolt"></i>
           <a href="http://www.google.com">Tech</a>
         </div>
-
-        <Anchor targetOffset="65">
-          <Link href="#hero" title="Home" />
-          <Link href="#about" title="About" />
-          <Link href="#feature" title="Features" />
-          <Link href="#works" title="How it works" />
-          <Link href="#faq" title="FAQ" />
-          <Link href="#pricing" title="Pricing" />
-          <Link href="#contact" title="Contact" />
-        </Anchor>
+        <div className="mobileHidden">
+          <Anchor targetOffset="65">
+            <Link href="#hero" title="Home" />
+            <Link href="#about" title="About" />
+            <Link href="#feature" title="Features" />
+            <Link href="#works" title="How it works" />
+            <Link href="#faq" title="FAQ" />
+            <Link href="#pricing" title="Pricing" />
+            <Link href="#contact" title="Contact" />
+          </Anchor>
+        </div>
+        <div className="mobileVisible">
+          <Button type="primary" onClick={showDrawer}>
+            <i className="fas fa-bars"></i>
+          </Button>
+          <Drawer
+            // title="Basic Drawer"
+            placement="right"
+            closable={false}
+            onClose={onClose}
+            visible={visible}
+          >
+            <Anchor targetOffset="65">
+              <Link href="#hero" title="Home" />
+              <Link href="#about" title="About" />
+              <Link href="#feature" title="Features" />
+              <Link href="#works" title="How it works" />
+              <Link href="#faq" title="FAQ" />
+              <Link href="#pricing" title="Pricing" />
+              <Link href="#contact" title="Contact" />
+            </Anchor>
+          </Drawer>
+        </div>
       </div>
     </div>
   );
